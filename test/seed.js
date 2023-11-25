3ace97914a4e53374b200b26676c4952ab22bb2b
noah <wags0819@gmail.com>
2023-11-25 16:03:02 -0500

    Update seed.js

--- test/seed.js
+++ test/seed.js
@@ -4,12 +4,16 @@ import { dbConnection, closeConnection } from '../config/mongoConnection.js';
 const db = await dbConnection();
 await db.dropDatabase();
 
+// reviewsforproducts
+console.log('-----------------------');
+console.log('addreview');
 const review1 = await reviewsforproductsData.addReview({
     user_id: "1",
     product_id: "1",
     review: "This is a review",
     rating: 5
 });
+console.log('addreview');
 const review2 = await reviewsforproductsData.addReview({
     user_id: "1",
     product_id: "2",
@@ -17,7 +21,8 @@ const review2 = await reviewsforproductsData.addReview({
     rating: 5
 });
 
-const review3 = await reviewsforstoresData.addReview({
+console.log('addreview');
+const review3 = await reviewsforproductsData.addReview({
     user_id: "1",
     store_id: "1",
     review: "This is a review",
@@ -25,24 +30,24 @@ const review3 = await reviewsforstoresData.addReview({
 });
 
 const getAllReviews = await reviewsforproductsData.getAllReviews();
-console.log(getAllReviews);
+console.log('getallreviews',getAllReviews);
 
-const getReviewById = await reviewsforproductsData.getReviewById(review1._id);
+const getReviewById = await reviewsforproductsData.getReviewById(review1._id.toString());
 console.log('getReviewById', getReviewById);
 
-const removeReview = await reviewsforproductsData.removeReview(review2._id);
+const removeReview = await reviewsforproductsData.removeReview(review2._id.toString());
 console.log('removeReview', removeReview);
 const getAllReviews2 = await reviewsforproductsData.getAllReviews();
 console.log(getAllReviews2);
 
-const updateReview = await reviewsforproductsData.updateReview(review1._id, {
+const updateReview = await reviewsforproductsData.updateReview(review1._id.toString(), {
     user_id: "1",
     product_id: "1",
     review: "This is a updated review",
     rating: 1
 });
-const getReviewById2 = await reviewsforproductsData.getReviewById(review1._id);
-console.log('getReviewById2', getReviewById2);
+const getReviewById2 = await reviewsforproductsData.getReviewById(review1._id.toString());
+console.log('updateReview', getReviewById2);
 
 console.log('-----------------------');
 console.log('done seeding db');
