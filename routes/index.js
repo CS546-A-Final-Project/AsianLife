179bbe72322962af0b9184900c8ab46025689630
noah <wags0819@gmail.com>
2023-11-30 19:12:02 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@
 import loginRoutes from './login.js';
 import registerRoutes from './register.js';
 import homeRoutes from './home.js';
-
+import storeRoutes from './store.js';
 const constructorMethod = (app) => {
   app.use('/', loginRoutes);
   app.use('/register', registerRoutes);
   app.use('/home', homeRoutes)  
-
+  app.use('/store', storeRoutes);
   app.use('*', (req, res) => {
     res.status(404).render('error', { title: "404 NOT FOUND", error: "404 NOT FOUND" });
   });
