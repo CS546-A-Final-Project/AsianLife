117311309db289e4ce3a0b75281a11ba7a6719c2
bobwen0205 <wenmingzhi10@gmail.com>
2023-12-05 03:51:23 -0500

    Initial Commit to My branch

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import { users } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
+import { getUser } from '../data/users.js';
 const router = express.Router();
 
 router.route('/').get(async (req, res) => {
-
+    const title = "Home Page";
+    const id = req.session.user.id;
+    const user = await getUser(id);
+    const name = user.userName;
+    res.status(200).render('home',{
+        title: title, 
+        name: name,
+        avatarId: user.avatar,
+    })
 });
 
 router.route('/:id').get(async (req, res) => {
