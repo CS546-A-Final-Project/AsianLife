837e44490b6f6c11a8ea7a6caf40d78f6a3b552b
Noah <112596056+Noah-wu0819@users.noreply.github.com>
2023-12-07 13:55:10 -0500

    Update products.js

--- data/products.js
+++ data/products.js
@@ -50,6 +50,9 @@ const updateProduct = async (id, updatedProduct) => {
     if (updatedProduct.product_reviews) {
         updatedProductData.product_reviews = updatedProduct.product_reviews;
         }
+    if (updatedProduct.product_image) {
+        updatedProductData.product_image = updatedProduct.product_image;
+        }
     let updateCommand = {
         $set: updatedProductData,
         };
