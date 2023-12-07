b85b934cb8379b99f02f8d613382761ed44696b1
noah <wags0819@gmail.com>
2023-12-07 17:03:05 -0500

    Update editstore.js

--- routes/editstore.js
+++ routes/editstore.js
@@ -1,8 +1,17 @@
 import express from 'express';
 const router = express.Router();
 
-router.route('/').get(async (req, res) => {
-    return res.json('edit store page');
+router.route('/:id').get(async (req, res) => {
+    const storeId = req.params.id;
+    try {
+        if (!ObjectId.isValid(storeId)) {
+            throw 'invalid object ID';
+        }
+    } catch (e) {
+        return res.status(400).render('error', { error: e });
+    }
+
+    return res.render('editstore',{title: "Edit Store"});
 });
 
 export default router;
