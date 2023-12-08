e86340b81ca7ad8a8f1b3a1856f691e7a36b0bd5
noah <wags0819@gmail.com>
2023-12-07 19:30:50 -0500

    Update editstore.js

--- routes/editstore.js
+++ routes/editstore.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ObjectId } from 'mongodb';
+
 const router = express.Router();
 
 router.route('/:id').get(async (req, res) => {
@@ -10,8 +12,8 @@ router.route('/:id').get(async (req, res) => {
     } catch (e) {
         return res.status(400).render('error', { error: e });
     }
-
-    return res.render('editstore',{title: "Edit Store"});
+    console.log(storeId);
+    return res.render('editstore',{title: "Edit Store", storeId: storeId});
 });
 
 export default router;
