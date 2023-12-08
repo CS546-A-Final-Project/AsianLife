6de4a617997bb1ca231d5ea6532717dea20395dd
bobwen0205 <wenmingzhi10@gmail.com>
2023-12-08 03:53:17 -0500

    Update front_end_validation.js

--- public/js/front_end_validation.js
+++ public/js/front_end_validation.js
@@ -209,4 +209,90 @@
             }
         });
     }
+
+    const addStoreStaticForm = document.getElementById('add-store-form');
+
+    if (addStoreStaticForm) {
+        const addressInput = document.getElementById('address');
+        const cityInput = document.getElementById('city');
+        const stateInput = document.getElementById('state');
+        const zipCodeInput = document.getElementById('zipCode');
+        const phoneNumberInput = document.getElementById('phoneNumber');
+        const emailAddressInput = document.getElementById('email');
+
+        addStoreStaticForm.addEventListener('submit', (event) => {
+            let errors = [];
+            errorContainer.innerHTML = '';
+
+            const address = addressInput.value.trim();
+            const city = cityInput.value.trim();
+            const state = stateInput.value.trim();
+            const zipCode = zipCodeInput.value.trim();
+            const phoneNumber = phoneNumberInput.value.trim();
+            const emailAddress = emailAddressInput.value.trim();
+
+            addressInput.value = address;
+            cityInput.value = city;
+            stateInput.value = state;
+            zipCodeInput.value = zipCode;
+            phoneNumberInput.value = phoneNumber;
+            emailAddressInput.value = emailAddress;
+
+            const addressRegex = /^[a-zA-Z0-9\s\-,]+$/;
+            if (!addressRegex.test(address)) {
+                errors.push("Address contains invalid symbols");
+            }
+            if (address.length < 3) {
+                errors.push("Address is less than 3 characters");
+            }
+
+            const cityRegex = /^[a-zA-Z\s\-]+$/;
+            if (!cityRegex.test(city)) {
+                errors.push("City contains invalid symbols");
+            }
+            if (city.length < 3) {
+                errors.push("City is less than 3 characters");
+            }
+
+            const states = [
+                "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+                "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+                "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+                "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+                "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+            ];
+            if (!states.includes(state.toUpperCase())) {
+                errors.push("State must be valid selected from the list");
+            }
+
+            if (zipCode.length !== 5) {
+                throw "Zip must contain 5 numbers";
+            }
+            
+            for(let i of zipCode) {
+                if(i < '0' || i > '9') {
+                    errors.push("Zip must contain only numbers!");
+                }
+            }
+
+            const phoneRegex = /^\d{10}$/;
+            if (!phoneRegex.test(phoneNumber)) {
+                errors.push("US phone number must contain 10 digits numbers");
+            }
+
+            const emailAddressRegex = /\S+@\S+\.\S+/;
+            if (!emailAddressRegex.test(emailAddress)) {
+                errors.push("Email address should be a valid email address format. example@example.com");
+            }
+
+            if (errors.length > 0) {
+                event.preventDefault();
+                for (let i = 0; i < errors.length; i++) {
+                    const addLi = document.createElement('li');
+                    addLi.textContent = errors[i];
+                    errorContainer.appendChild(addLi);
+                }
+            }
+        });
+    }
 })();
