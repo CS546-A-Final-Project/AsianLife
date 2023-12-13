9312cea31986a48de3016d43ff7afe2be094b1ee
Siyuan Fang <funsiyuan@gmail.com>
2023-12-12 23:35:28 -0500

    updated

--- routes/commentsDetail.js
+++ routes/commentsDetail.js
@@ -4,29 +4,57 @@ import { commentsforstoresData } from "../data/index.js";
 const router = express.Router();
 
 
-router.route('/').get(async (req, res) => {
+router.route('/:id').get(async (req, res) => {
     let user = req.session.user//和佳俊对接
+    let storeId = req.params.id
     let isAdmin = true;
-    if(req.body.user_id !== 'admin') isAdmin = false;
+    // let isCommentuser = true; ---how
+    if(user.role !== 'admin') {
+      isAdmin = false;
+    }
     res.render("/commentDetail", {comment: comment, answer: answer, isAdmin: isAdmin})
     })
     .post(async (req, res) => {
-        let {_id, user_id, store_id, comment, answer, answerInput} = req.body //和佳俊对接
-        let isAdmin = true;
-        if(req.body.user_id !== 'admin') isAdmin = false;
+        // let {_id, user_id, store_id, comment, answer, answerInput} = req.body //和佳俊对接
+        let user = req.session.user;
+        let id = req.params.id
+        // let rating = storesData.getStoreById(store_id).rating;
+        // let storeName = storesData.getStoreById(store_id).name;
+        let answerInput = req.body.answerInput;
+        // let isUser = true;
+        // let isAdmin = true;
+        // if(req.body.user_id !== 'admin') isAdmin = false;
 
         try{
             checkString(answerInput,"answer");
         }catch(e){
-            return res.status(400).render('error', {title: "Error", message: e})
+            return res.status(400).render('error', {title: "Error", message: "please enter valid answer"})
         }
 
         try{
-            const newAnswer = await commentsforstoresData.addAnswer(_id, answerInput)
-            res.render("/commentDetail", {comment: newAnswer.comment, answer: newAnswer.answer, isAdmin: isAdmin},)
+            const newAnswer = await commentsforstoresData.addAnswer(id, answerInput)
+            res.redirect(`/${id}`)
         }catch(e){
             return res.status(500).render('error', {title: "Error", message:"Internal Server Error"})
         }
+      })
+      .update(async (req, res) => {
+          // let {_id, user_id, store_id, comment, answer, answerInput} = req.body //和佳俊对接
+          let user = req.session.user;
+          let id = req.params.id
+          // let rating = storesData.getStoreById(store_id).rating;
+          // let storeName = storesData.getStoreById(store_id).name;
+          
+          // let isUser = true;
+          // let isAdmin = true;
+          // if(req.body.user_id !== 'admin') isAdmin = false;
+  
+          try{
+              const newAnswer = await commentsforstoresData.deleteAnswer(id)
+              res.redirect(`/${id}`)
+          }catch(e){
+              return res.status(500).render('error', {title: "Error", message:"cannot delete this answer"})
+          }
     });
 
 export default router;
