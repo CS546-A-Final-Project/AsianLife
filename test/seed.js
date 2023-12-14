9656d6f018656adc2d458be70bdcac217c64dbd0
bobwen0205 <wenmingzhi10@gmail.com>
2023-12-14 18:41:56 -0500

    Update seed.js

--- test/seed.js
+++ test/seed.js
@@ -11,53 +11,74 @@ import { dbConnection, closeConnection } from "../config/mongoConnection.js";
 const db = await dbConnection();
 await db.dropDatabase();
 
-let newUser = usersData.addUser({
-  userName: 'bobwen',
-  firstName: 'Mingzhi',
-  lastName: 'Wen',
-  email: 'wenmingzhi0@gmail.com',
-  password: 'Abc123,,',
-  role: 'admin'
+let newUser = await usersData.addUser(
+  'bobwen',
+  'Mingzhi',
+  'Wen',
+  'abc1@gmail.com',
+  'Abc123,,',
+  'admin'
+);
+let user1Id = newUser.newUserId;
+await addStore({
+  adminId: user1Id,
+  address: "address",
+  city: "Hoboken",
+  state: "NJ",
+  zipCode: "07030",
+  phoneNumber: "1234567890",
+  email: "abc1@gmail.com",
 });
+
+newUser = await usersData.addUser(
+  'bobwen',
+  'Mingzhi',
+  'Wen',
+  'abc2@gmail.com',
+  'Abc123,,',
+  'admin'
+);
+let user2Id = newUser.newUserId;
+await addStore({
+  adminId: user2Id,
+  address: "address",
+  city: "Hoboken",
+  state: "NJ",
+  zipCode: "07030",
+  phoneNumber: "1234567890",
+  email: "abc1@gmail.com",
+});
+
+newUser = await usersData.addUser(
+  'bobwen',
+  'Mingzhi',
+  'Wen',
+  'abc3@gmail.com',
+  'Abc123,,',
+  'admin'
+);
+let user3Id = newUser.newUserId;
 console.log(newUser);
 
-// newUser = usersData.addUser({
-//   userName: 'bobwen1',
-//   firstName: 'Mingzhi',
-//   lastName: 'Wen',
-//   email: 'wenmingzhi1@gmail.com',
-//   password: 'Abc123,,',
-//   role: 'admin'
-// });
-// console.log(newUser);
+newUser = await usersData.addUser(
+  'bobwen',
+  'Mingzhi',
+  'Wen',
+  'abc4@gmail.com',
+  'Abc123,,',
+  'admin'
+);
+let user4Id = newUser.newUserId;
+console.log(newUser);
 
-// newUser = usersData.addUser({
-//   userName: 'bobwen',
-//   firstName: 'Mingzhi',
-//   lastName: 'Wen',
-//   email: 'wenmingzhi2@gmail.com',
-//   password: 'Abc123,,',
-//   role: 'admin'
-// });
-// console.log(newUser);
-
-// newUser = usersData.addUser({
-//   userName: 'bobwen',
-//   firstName: 'Mingzhi',
-//   lastName: 'Wen',
-//   email: 'wenmingzhi3@gmail.com',
-//   password: 'Abc123,,',
-//   role: 'admin'
-// });
-// console.log(newUser);
-
-// newUser = usersData.addUser({
-//   userName: 'bobwen',
-//   firstName: 'Mingzhi',
-//   lastName: 'Wen',
-//   email: 'wenmingzhi4@gmail.com',
-//   password: 'Abc123,,',
-//   role: 'admin'
-// });
+newUser = await usersData.addUser(
+  'bobwen',
+  'Mingzhi',
+  'Wen',
+  'abc5@gmail.com',
+  'Abc123,,',
+  'admin'
+);
+let user5Id = newUser.newUserId;
 console.log(newUser);
 await closeConnection();
