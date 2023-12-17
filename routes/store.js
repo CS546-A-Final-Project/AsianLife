4cbd44ae1b7c3ca33d6ec0a036318d71f22f6467
bobwen0205 <wenmingzhi10@gmail.com>
2023-12-16 22:11:29 -0500

    Update store.js

--- routes/store.js
+++ routes/store.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { getUser } from '../data/users.js';
 import { ObjectId } from "mongodb";
 import { storesData, productsData } from "../data/index.js";
+import { getAllStores } from '../data/stores.js';
 const router = express.Router();
 
 router.route('/:id').get(async (req, res) => {
@@ -18,7 +19,7 @@ router.route('/:id').get(async (req, res) => {
         const store = await storesData.getStoreById(storeId);
         const storeProducts = await productsData.getAllProductsByStoreId(storeId);
         storeProducts.forEach(product => {
-            if(product.productReviews[0] && product.productReviews[0].productReviews)
+            if (product.productReviews[0] && product.productReviews[0].productReviews)
                 product.firstReview = product.productReviews[0].productReviews;
             else
                 product.firstReview = "No Review";
@@ -34,18 +35,18 @@ router.route('/:id').get(async (req, res) => {
         if (role === 'admin' && user.ownedStoreId && storeId !== user.ownedStoreId) {
             isAdminAndHasAStore = true;
         }
-        const productReviews = 
-        res.status(200).render('store', {
-            name: theUser.userName,
-            avatarId: theUser.avatar,
-            storeId: user.ownedStoreId,
-            isAdminAndHasAStore: isAdminAndHasAStore,
-            title: store.name,
-            storeProducts: storeProducts,
-            storeID: storeId,
-            user: userRole,
-            isOwner: isOwner
-        });
+        const productReviews =
+            res.status(200).render('store', {
+                name: theUser.userName,
+                avatarId: theUser.avatar,
+                storeId: user.ownedStoreId,
+                isAdminAndHasAStore: isAdminAndHasAStore,
+                title: store.name,
+                storeProducts: storeProducts,
+                storeID: storeId,
+                user: userRole,
+                isOwner: isOwner
+            });
 
 
     } catch (e) {
@@ -53,4 +54,41 @@ router.route('/:id').get(async (req, res) => {
     }
 });
 
+router.route('/').get(async (req, res) => {
+    const title = "All Store";
+    const id = req.session.user.id;
+    const role = req.session.user.role;
+    const storeId = req.session.user.ownedStoreId;
+    let isAdminAndHasAStore = false;
+    if (role === 'admin' && storeId) {
+        isAdminAndHasAStore = true;
+    }
+    let isAdminAndHasNoStore = false;
+    if (role === 'admin' && !storeId) {
+        isAdminAndHasNoStore = true;
+    }
+    const user = await getUser(id);
+    const name = user.userName;
+    const stores = await getAllStores();
+    if (!stores) {
+        return res.status(400).render('storeList',{
+            title: title, 
+            name: name,
+            avatarId: user.avatar,
+            hasStores: false,
+            isAdminAndHasAStore: isAdminAndHasAStore,
+            isAdminAndHasNoStore: isAdminAndHasNoStore,
+        });
+    }
+    res.status(200).render('storeList',{
+        title: title, 
+        name: name,
+        avatarId: user.avatar,
+        hasStores: true,
+        stores: stores,
+        isAdminAndHasAStore: isAdminAndHasAStore,
+        isAdminAndHasNoStore: isAdminAndHasNoStore,
+        storeId: storeId,
+    })
+})
 export default router;
