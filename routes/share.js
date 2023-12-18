894a7a6e332908678718645519f014d788fe0b90
Yancy Luo <yancyluo936@gmail.com>
2023-12-17 20:52:56 -0500

    update

--- routes/share.js
+++ routes/share.js
@@ -103,15 +103,6 @@ router.post('/', async (req, res) => {
             recommendedProducts: topRatedProducts,
             isAdminAndHasAStore: isAdminAndHasAStore,
             isAdminAndHasNoStore: isAdminAndHasNoStore,
-        res.render('home', {
-            title: "Home",
-            name: name,
-            avatarId: user.avatar,
-            message: "Promotion email sent successfully",
-            recommendedStores: topRatedStores,
-            recommendedProducts: topRatedProducts,
-            isAdminAndHasAStore: isAdminAndHasAStore,
-            isAdminAndHasNoStore: isAdminAndHasNoStore,
         });
     } catch (error) {
         console.error('Failed to send email', error);
